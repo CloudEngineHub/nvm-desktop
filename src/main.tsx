1bd50fa847d81f402c89844c4ae4fd93651a4e59
The1111mp <The1111mp@outlook.com>
2025-01-23 10:17:54 +0800

    fix: set the theme in advance at startup to prevent flickering
    Signed-off-by: The1111mp <The1111mp@outlook.com>

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,24 @@ import App from './app';
 
 import { getSettings } from '@/services/cmds';
 import { getSystemCurrentTheme } from '@/services/api';
+import { applyTheme } from './lib/utils';
+import { Themes } from './types';
 
 /**
  * Get user settings data & system theme in advance.
  * Can solve the view flickering caused by refresh (caused by theme).
  * This would have increased the delay in loading the first screen.
  * However, thanks to the powerful performance of `rust`, the time consumption is within an acceptable range.
- * The delay is always within `15ms` (in development)
+ * The delay is always within `15ms` (in development).
  */
 (async () => {
   const [settings, sysTheme] = await Promise.all([
     getSettings(),
     getSystemCurrentTheme(),
   ]);
+
+  // Set the theme in advance to prevent flickering.
+  applyTheme(settings.theme !== Themes.System ? settings.theme : sysTheme);
 
   createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
